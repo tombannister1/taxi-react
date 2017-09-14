ae7fc01bc94a8f8fd86e3e22a7033ca1939566f3
Tom Bannister <tomb@Adprinciples.co.uk>
2017-09-14 17:22:51 +0100

    set up project so android and ios read from the same file

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+class Main extends React.Component {
+
+    render() {
+        return (
+            <View>
+                <Text>Taxi App</Text>
+            </View>
+        );
+
+    }
+
+}
+
+export default Main;
